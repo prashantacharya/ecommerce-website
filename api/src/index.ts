c50ecd3ce4b6ec0d2181a7d1ce020f4058898ccc
Prashant <prashantacharya@lftechnology.com>
2021-08-31 21:10:18 +0545

    Configure error handling middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import apiRouter from './api.routes';
+import createError from './utils/createError';
 
 import { config } from 'dotenv';
 config();
@@ -12,6 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use('/api/v1/', apiRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404, 'Route not found'));
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  res.statusCode = error.statusCode || 503;
+  res.send({
+    status: 'Error',
+    message: error.message || 'Something went wrong',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
---
+++ api/src/utils/createError.ts
@@ -0,0 +1,15 @@
+class CustomError extends Error {
+  message: string;
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+const createError: any = (message: string, statusCode: number) => {
+  return new CustomError(message, statusCode);
+};
+
+export default createError;
